0db3d5df20338e64375bd9d0165361bbf4f5e606
muhibush <ukiacrew@gmail.com>
2023-09-13 15:53:25 +0700

    setup ScreenUtil

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mr_white/utils/navigator/routes.dart';
 import 'generated/l10n.dart';
 
@@ -12,21 +13,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: S.current.mrWhite,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      onGenerateRoute: Routes.getRouteGenerate,
-      builder: (context, child) => child ?? const SizedBox(),
-    );
+    return ScreenUtilInit(
+
+        /// TODO //Set the fit size (Find your UI design, look at the dimensions of the device screen and fill it in,unit in dp)
+        /// add mobile and tablet if needed
+        designSize: const Size(360, 640),
+        builder: (context, child) {
+          return MaterialApp(
+            title: S.current.mrWhite,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            localizationsDelegates: const [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: S.delegate.supportedLocales,
+            onGenerateRoute: Routes.getRouteGenerate,
+            builder: (context, child) {
+              return MediaQuery(
+                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+                child: Builder(
+                  builder: (context) => child ?? const SizedBox.shrink(),
+                ),
+              );
+            },
+          );
+        });
   }
 }
